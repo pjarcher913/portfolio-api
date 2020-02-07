f49eacba314e9fce4fafe81a92016b66bfe1245b
Patrick Archer <37312105+pjarcher913@users.noreply.github.com>
2020-02-07 12:44:46 -0700

    Re-organize directories and build destinations
    Renamed ~/API-ARCHER/ to ~/src/. Removed ~/src/src/. Moved build output folder to outside ~/src/. Modified ~/src/main.go to identify static site files in ~/src/web/.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# website-golang-reactjs
- My website, with backend serving by Golang and frontend operation by ReactJS.
+#pjarcher913/portfolio-api
+ Full-stack Golang/React project that serves my portfolio website with an underlying API.
+ The API is entirely composed of Easter eggs...different requests will do different things.
+ See [this link](#) for more information on the API.
---
+++ src/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const SERVER_PORT = ":3000"  // Host port to serve on
+const WEB_DIR = "./src/web"  // Where the static production build can be found for the website frontend
+
+func main() {
+	// Perform all setups before server goes live and starts listening for requests
+	initialSetup()
+	// Begin serving the site and listening on assigned web port
+	fmt.Println("Server started on 127.0.0.1" + SERVER_PORT)
+	startServer()
+}
+
+func startServer() {
+	http.Handle("/", http.FileServer(http.Dir(WEB_DIR)))
+	http.ListenAndServe(SERVER_PORT, nil)
+}
+
+func initialSetup() {
+	fmt.Println("Initializing services.")
+	logger("_", "Service initialized. Executing initialSetup().")
+}
+
+// TODO: Find alternative logging implementation that doesn't require opening and closing the log file every time.
+func logger(flag string, msg string) {
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		//log.Fatal(err)
+		fmt.Printf("[CRITICAL] Error during logger() execution. Stopping program execution.")
+		os.Exit(1)
+	}
+	log.SetOutput(file)
+
+	switch flag {
+	case "w":
+		// Warning case
+		log.Printf("[WARN]", msg)
+	case "e":
+		// Error case
+		log.Printf("[ERROR]", msg)
+	case "c":
+		// Critical case
+		log.Printf("[CRITICAL]", msg)
+	default:
+		log.Printf(msg)
+	}
+
+	file.Close()
+}
